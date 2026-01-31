631a84033050cd5da531036144770a3c785db9e8
esengooo <esengo.artist@gmail.com>
2026-01-31 18:29:06 +0100

    Implement toggleBio function for bio display
    Add toggleBio function to show/hide bio content

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+function toggleBio() {
+  const moreBio = document.getElementById("more-bio");
+  const btn = document.querySelector(".read-more-btn");
+
+  if (moreBio.style.display === "block") {
+    moreBio.style.display = "none";
+    btn.textContent = "Read more";
+  } else {
+    moreBio.style.display = "block";
+    btn.textContent = "Read less";
+  }
+}
